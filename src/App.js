ebf73219bca0d95f6f9f98fad8524d6c4dbe7749
sokkanji <sskkanji@gmail.com>
2021-04-08 16:45:09 +0900

    console log 확인

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
-function App() {
-  return <PhoneForm />;
+class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 
 class PhoneForm extends Component {
     state = {
-        name: ''
+        name: '',
+        phone: ''
     }
     handleChange = (e) => {
         this.setState({
-            name: e.target.value
-        })
+            [e.target.name]: e.target.value
+        });
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        this.setState({
+            name: '',
+            phone: ''
+        });
     }
     render() {
         return (
             <form>
-                <input value={this.state.name} placeholder="이름" onChange={this.handleChange} />
-                <div>{this.state.name}</div>
+                <input value={this.state.name} placeholder="이름" onChange={this.handleChange} name="name" />
+                <input value={this.state.phone} placeholder="전화번호" onChange={this.handleChange} name="phone" />
+                <button type="submit" onClick={this.handleSubmit}>등록</button>
             </form>
         );
     }
