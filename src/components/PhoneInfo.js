61568b56ad95a7a212d23a6ae9d43b2f74c3f149
sokkanji <s2018s07@e-mirim.hs.kr>
2021-04-09 16:37:04 +0900

    불변성을 지키는 이유와 업데이트 최적화

--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -15,6 +15,17 @@ class PhoneInfo extends Component {
         phone: '',
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        // 수정 상태가 아니고, info 값이 같다면 리렌더링 안함
+        if (!this.state.editing  
+            && !nextState.editing
+            && nextProps.info === this.props.info) {
+          return false;
+        }
+        // 나머지 경우엔 리렌더링함
+        return true;
+      }
+
     handleRemove = () => {
         const { info, onRemove } = this.props;
         onRemove(info.id);
@@ -50,6 +61,7 @@ class PhoneInfo extends Component {
       }
 
     render() {
+        console.log('render PhoneInfoList' + this.props.info.id);
         const style = {
             border: '1px solid black',
             padding: '8px',
